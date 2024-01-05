3b4fd03d68cd016c0263baf5cf480f15e0f29fc7
DHEERAJ M <148963738+itsthedheeraj@users.noreply.github.com>
2024-01-06 01:19:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document
+    .getElementById("contactme")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+      alert("Thank you for signing up for the free trial!");
+  });
+
+  $(document).ready(function() {
+  $(".fa-bars").click(function() {
+    $(this).toggleClass("fa-times");
+    $(".navbar").toggleClass("nav-toggle");
+  });
+
+  $(window).on('scroll load', function () {
+    $('.fa-bars').removeClass('fa-times');
+    $('.navbar').removeClass('nav-toggle');
+
+    if ($(window).scrollTop() > 30) {
+      $('header').addClass('header-active');
+    } else {
+      $('header').removeClass('header-active');
+    }
+
+  });
+});
